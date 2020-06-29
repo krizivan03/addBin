d6c5b52191272ace7b7514e52a7eca5b931084a7
Christopher Canales <krizivan03@yahoo.com>
2020-06-28 19:52:54 -0700

    Initial Commit

---
+++ addBin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  addBin
+//
+//  Created by Christopher Canales on 6/28/20.
+//  Copyright © 2020 Christopher Canales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
